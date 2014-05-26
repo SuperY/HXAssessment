cbc6c4f019d2775248430984e2a4d4237014df89
Felix_Y <mac@felix.lan>
2014-05-26 18:42:02 +0800

    Initial Commit

---
+++ HXAssessment/HXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HXAppDelegate.h
+//  HXAssessment
+//
+//  Created by Felix_Y on 14-5-26.
+//  Copyright (c) 2014年 HX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HXAssessment/HXViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXViewController.h
+//  HXAssessment
+//
+//  Created by Felix_Y on 14-5-26.
+//  Copyright (c) 2014年 HX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXViewController : UIViewController
+
+@end
